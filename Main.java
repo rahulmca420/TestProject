84cac8e3a58eb4eff86fbc4febe86506851595f5
rahulmca420 <77891385+rahulmca420@users.noreply.github.com>
2026-01-18 19:39:26 +0530

    initial commit
    aaa

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String strs[] = {"eat","tea","tan","ate","nat","bat"};
+        int nums[]={1,1,1,2,2,3};
+
+        System.out.println(Arrays.toString(topKFrequent(nums,2)));
+    }
+    public static int[] topKFrequent(int[] nums, int k) {
+        if(k == nums.length){
+            return nums;
+        }
+        Map<Integer,Integer> hashMap = new HashMap<>();
+        for(int c:nums){
+            hashMap.put(c, hashMap.getOrDefault(c,0)+1);
+        }
+        Queue<Integer> queue=new PriorityQueue<>((a,b)-> hashMap.get(a)-hashMap.get(b));
+        for (int n:hashMap.keySet()){
+            queue.add(n);
+            if(queue.size() >k){
+                queue.poll();
+            }
+        }
+        System.out.println(queue);
+        int res[]=new int[k];
+        for(int i=0;i<k;i++){
+            res[i]=queue.poll();
+        }
+
+        System.out.println(queue);
+		System.out.println("hi");
+		
+
+      return res;
+    }
+}
